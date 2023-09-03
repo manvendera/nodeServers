42120b297bf955050e7ffb5624057ffa53f42bae
manvendera pathak <120110556+manvendera@users.noreply.github.com>
2023-09-03 11:12:33 +0530

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,11 @@
+const os = require('os');
+// console.log("Cpu Arch" +os.arch());
+// console.log("Free memory"+ os.freemem);
+// console.log("Total memeory" + os.totalmem);
+// console.log("network interfaces"+ JSON.stringify(os.networkInterfaces()));
+// console.log("os default temp dir" +os.tmpdir);
+
+console.log(" endianess"+ os.endianness);
+console.log("Hostname" +os.hostname);
+console.log("os type"+ os.type);
+console.log("platform" +os.platform);
