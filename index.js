002e812903fdf4cae4713c5f5a5cd615bb33f7f9
manvendera pathak <120110556+manvendera@users.noreply.github.com>
2023-09-03 14:17:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,72 +1,18 @@
-const fs = require('fs');
-const { buffer } = require('stream/consumers');
-// console.log("Read START");
-// asynchronous way to read file data
-// fs.readFile('input.txt',function(err,data){
-//     if (err){
-//         console.log("Error:",err,data);
-//         return err;
-//     }
-//     console.log("Data :", data.toString());
-//     console.log("Read End");
-//     return data;
-// })
+const expess = require("express");
+const app = expess();
+const PORT = 5000;
+const HOSTNAME = "localhost";
 
-// console.log("Read last");
+app.get("/", (req, res) => {
+  res.send("HOME PAGE");
+});
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+app.get("/contact", (req, res) => {
+  res.send("contact page");
+});
 
-// synchronous way to read file data
-// var data = fs.readFileSync('input.txt');
-// console.log("Data : ", data.toString());
-// console.log("Read first");
-// console.log("Read last");
-
-
-const buf = new Buffer(1024);
-
-// fs.open('input.txt', 'r+', function(err,fd){
-//     if(err){
-//         console.log("Error in opening file: ", err);
-//     }
-//     console.log("File open successfully");
-
-//     fs.read(fd,buf, 0,buf.length, 0,function(err,bytes){
-//         if(err){
-//             console.log("Error in readin file");
-//         }
-//         console.log("Data: ", bytes);
-//         console.log("Data :" ,buf.slice(0 , bytes).toString());
-//         fs.close(fd, function(err){
-//             if(err){
-//                 console.log("Error in closing file");
-//             } else {
-//                 console.log("Success in closing file");
-//             }
-//         })
-//     })
-// })
-//  writing text into file
-
-// fs.writeFile("input.txt",'manvendera pathak' ,function(err){
-//     if (err){
-//         console.log("Errorin writing file");
-//     } else 
-//     console.log("Success in writing file");
-// })
-//  appnd in file
-
-// fs.appendFile('input.txt',' is a student of computer science', function(err){
-//     if(err){
-//         console.log("Errror in appendein file");
-//     }
-//     else{
-//         console.log("Sucess in append file");
-//     }
-// })
-fs.unlink('input.txt',function(err){
-    if(err){
-        console.log("Error in delting file");
-    }
-    else{
-        console.log("Sucess in deleting file");
-    }
-})
+app.listen(PORT, () => {
+  console.log(`server running at ${HOSTNAME}:${PORT}`);
+});
